f36dfd5c51f61c72653daf78015184d2447844df
oliveraton1234 <61020652+oliveraton1234@users.noreply.github.com>
2020-10-08 10:25:42 -0500

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,3 @@
-  
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,52 +14,52 @@ import java.util.Random;
 public class Main {
     
     public static class Filosofo{
-        int tiempo_comida;
-        int tiempo_hambre;
-        boolean comiendo = false;
+        int tiempo_comida; // tiempo que se tarda en comer
+        int tiempo_hambre; // tiempo que dura en tener hambre sin morir
+        boolean comiendo = false;  //  turno para comer
     }
     
     public static Filosofo[] iniciar(Filosofo lista[]){
         Random rand = new Random();
         for(int i = 0; i < 8; i++){
-            lista[i] = new Filosofo();
-            lista[i].tiempo_comida =  rand.nextInt(100);
-            lista[i].tiempo_hambre =  rand.nextInt(100);
-            if( (i + 1) % 3 == 0 ){
-                lista[i].comiendo = true;
+            lista[i] = new Filosofo(); // inicializamos cada filosofo
+            lista[i].tiempo_comida =  rand.nextInt(100); // asignamos un numero aleatorio para el tiempo que tarda en comer
+            lista[i].tiempo_hambre =  rand.nextInt(100); // asignamos un numero aleatorio para el tiempo que tiene para comer y no morir
+            if( (i + 1) % 3 == 0 ){  // cada 2 filosofos
+                lista[i].comiendo = true; //asignamos un turno
             }
         }
         return lista;
     }
     
     public static void escanear(Filosofo tiempos[], Filosofo cena[], int i){
-        if(i == 7){
-            if(tiempos[i].tiempo_comida <= 0 && tiempos[0].tiempo_hambre > 0){
-                tiempos[i].tiempo_comida = cena[i].tiempo_comida;
-                tiempos[i].comiendo = false;
-                tiempos[0].comiendo = true;
-                tiempos[0].tiempo_hambre = cena[0].tiempo_hambre;
+        if(i == 7){ // si es el ultimo, se lo pasamos al inicio de la "mesa" (arreglo)
+            if(tiempos[i].tiempo_comida <= 0 && tiempos[0].tiempo_hambre > 0){ //si termino de comer y el siguiente aun tiene hambre
+                tiempos[i].tiempo_comida = cena[i].tiempo_comida; // reiniciamos el tiempo de comida
+                tiempos[i].comiendo = false; // le quitamos el turno
+                tiempos[0].comiendo = true; // se pasa el turno al siguiente
+                tiempos[0].tiempo_hambre = cena[0].tiempo_hambre;  // reiniciamos el tiempo de hambre del filosofo
             }
         }else{
-            if(tiempos[i].tiempo_comida <= 0 && tiempos[i + 1].tiempo_hambre > 0){
-                tiempos[i].tiempo_comida = cena[i].tiempo_comida;
-                tiempos[i].comiendo = false;
-                tiempos[i + 1].comiendo = true;
-                tiempos[i + 1].tiempo_hambre = cena[i].tiempo_hambre;
+            if(tiempos[i].tiempo_comida <= 0 && tiempos[i + 1].tiempo_hambre > 0){ //si termino de comer y el siguiente aun tiene hambre
+                tiempos[i].tiempo_comida = cena[i].tiempo_comida;  // reiniciamos el tiempo de comida
+                tiempos[i].comiendo = false;  // le quitamos el turno
+                tiempos[i + 1].comiendo = true;  // se pasa el turno al siguiente
+                tiempos[i + 1].tiempo_hambre = cena[i].tiempo_hambre;  // reiniciamos el tiempo de hambre del filosofo
             }
         }
     }
     
     public static void imprimir(Filosofo lista[]){
         for(int i = 0; i < 8; i++){
-            if(lista[i].tiempo_hambre <= 0 && lista[i].comiendo == false){
-                System.out.println("El filosofo " + (i + 1) + " se murio de hambre.");
+            if(lista[i].tiempo_hambre <= 0 && lista[i].comiendo == false){ // si ya no tiene hambre y no esta comiendo
+                System.out.println("El filosofo " + (i + 1) + " se murio de hambre.");  // significa que murio
             }else{
-                if(lista[i].comiendo == false){
-                    System.out.println("El filosofo " + (i + 1) + " esta pensando.");
+                if(lista[i].comiendo == false){ // si no esta comiendo
+                    System.out.println("El filosofo " + (i + 1) + " esta pensando."); // esta esperando y pensando
                 }
-                else{
-                    System.out.println("El filosofo " + (i + 1) + " esta comiendo.");
+                else{ // si si esta comiendo
+                    System.out.println("El filosofo " + (i + 1) + " esta comiendo.");  // esta comiendo
                 }
             }
         }
@@ -68,32 +67,29 @@ public class Main {
     
     public static void main(String args[]) {
         // TODO code application logic here
-        Random rand = new Random();
-        Filosofo cena[] = new Filosofo[8];
-        cena = iniciar(cena);
-        Filosofo tiempos[] = cena;
-        int tiempo_cena = 9 + rand.nextInt(91);
+        Random rand = new Random(); // generar un numero aleatorio
+        Filosofo cena[] = new Filosofo[8]; // se pone la mesa de los filosofos
+        cena = iniciar(cena);  // iniciamos la cena
+        Filosofo tiempos[] = cena; // hacemos una copia de los tiempos de los filosofos
+        int tiempo_cena = 9 + rand.nextInt(91); // generamos un tiempo aleatorio en el que se pasaran los turnos
         
-        while(tiempo_cena > 0){
-            tiempo_cena--;
-            for(int i = 0; i < 8; i++){
-                if(tiempos[i].comiendo == true){
-                    tiempos[i].tiempo_comida--;
-                    escanear(tiempos, cena, i);
+        while(tiempo_cena > 0){  // mientras no se haya acabado el tiempo de cena
+            tiempo_cena--; // le quitamos un minuto
+            for(int i = 0; i < 8; i++){  // recorremos la mesa
+                if(tiempos[i].comiendo == true){  // si esta comiendo
+                    tiempos[i].tiempo_comida--;  // le quitamos un minuto al tiempo de comida
+                    escanear(tiempos, cena, i);  // y checamos si hay muertos, pensadores o comensales
                     
                 }
-                else{
-                    tiempos[i].tiempo_hambre--;
+                else{ // si no esta comiendo
+                    tiempos[i].tiempo_hambre--; // le quitamos un minuto al tiempo de espera
                 }
             }
-            if(tiempo_cena % 9 == 0){
-                imprimir(tiempos);
+            if(tiempo_cena % 9 == 0){  // cada 9 minutos
+                imprimir(tiempos);  // imprimimos el estado de la mesa
                 System.out.print("\n");
             }
         }
-        
-        
-        
         
     }
 }
